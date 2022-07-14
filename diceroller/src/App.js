5320a090e8d9a332e963ae30d3371e6932e4a27e
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-07-14 16:09:40 +1000

    committing

---
+++ diceroller/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dice from "./Components/dice"
+import React from "react"
+
+
+function App() {
+  return (
+    <Dice />
+  );
+}
+
+export default App;

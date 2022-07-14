e09b57a677ef2b884fbfa75dfc32504441539b0d
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-07-14 16:09:13 +1000

    setup

---
+++ diceroller/src/Components/dice.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Dices () {
+    return (
+    <div style=
+                {{width: 50, height: 50, backgroundColor: "yellow", position: 'absolute', color: 'black'}}>
+                    1
+        </div>
+    )
+}
+
+export default function Dice() {
+    return(
+        <div>
+           <Dices />
+        </div>
+    )
+}
